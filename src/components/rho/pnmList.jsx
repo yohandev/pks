f214958109c15293d4c48314b7b8ce56a5ba7bad
Yohan Guyomard <yohang@mit.edu>
2024-08-30 22:36:22 -0400

    new PNM button

--- src/components/rho/pnmList.jsx
+++ src/components/rho/pnmList.jsx
@@ -1,9 +1,10 @@
-import { getDatabase, ref as refDb } from "firebase/database";
+import { getDatabase, push as pushDb, ref as refDb, set as setDb } from "firebase/database";
 import { getStorage, ref as refStorage } from "firebase/storage";
 import { useFirebaseApp, useDatabase, useDownloadURL } from "solid-firebase";
 import { For, Match, Show, Switch } from "solid-js";
 
 import { PosterDb } from "./poster";
+import { useNavigate } from "@solidjs/router";
 
 function PnmList({ year }) {
     year = year ?? (new Date().getFullYear() + 4)
@@ -11,24 +12,47 @@ function PnmList({ year }) {
     const app = useFirebaseApp();
     const db = getDatabase(app);
     const pnms = useDatabase(refDb(db, `/rho/years/${year}`));
+    const navigate = useNavigate();
+
+    function addPnm(e) {
+        e.preventDefault();
+
+        const newPnmRef = pushDb(refDb(db, `/rho/people`));
+
+        newPnmRef.then((val) => {
+            const inYearRef = pushDb(refDb(db, `/rho/years/${year}`));
+            setDb(inYearRef, val.key);
+
+            navigate(`/rho/${val.key}`);
+        })
+
+        setDb(newPnmRef, { fullName: "new PNM!" });
+    }
 
     return (
-        <Switch>
-            <Match when={pnms.loading}>
-                <p>Loading...</p>
-            </Match>
-            <Match when={pnms.data}>
-                <For each={pnms.data}>{(uuid) =>
-                    <a href={`/rho/${uuid}`}>
-                        <PosterDb uuid={uuid} width="150px" />
-                    </a>
-                }</For>
-            </Match>
-            <Match when={pnms.error}>
-                <p>There was an error fetching PNMs...</p>
-                <p>{JSON.stringify(pnms.error)}</p>
-            </Match>
-        </Switch>
+        <div class="flex:column">
+            <form onSubmit={addPnm}>
+                <input type="submit" value="Add a new PNM"></input>
+            </form>
+            <div class="flex:row flex:wrap flex:justify-center">
+                <Switch>
+                    <Match when={pnms.loading}>
+                        <p>Loading...</p>
+                    </Match>
+                    <Match when={pnms.data}>
+                        <For each={Object.values(pnms.data)}>{(uuid) =>
+                            <a href={`/rho/${uuid}`} class="margin:5px">
+                                <PosterDb uuid={uuid} width="150px" />
+                            </a>
+                        }</For>
+                    </Match>
+                    <Match when={pnms.error}>
+                        <p>There was an error fetching PNMs...</p>
+                        <p>{JSON.stringify(pnms.error)}</p>
+                    </Match>
+                </Switch>
+            </div>
+        </div>
     );
 }
 
